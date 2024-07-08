9b0481429d5372dc7221decf56f599c89694e42c
Himanshu Rai <himanshuraimau9@gmail.com>
2024-07-08 11:26:28 +0530

    make a transaction

--- generate-keypair/generate-keypair.ts
+++ generate-keypair/generate-keypair.ts
@@ -1,11 +1,13 @@
-/*import { Keypair } from "@solana/web3.js";
+import { Keypair } from "@solana/web3.js";
 
 const keypair = Keypair.generate();
 
 console.log(`The public key is: `, keypair.publicKey.toBase58());
 console.log(`The secret key is: `, keypair.secretKey);
 console.log(`✅ Finished!`);
-*/
+
+
+/*
 import "dotenv/config"
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
@@ -13,4 +15,5 @@ const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
 console.log(
   `✅ Finished! We've loaded our secret key securely, using an env file!`
-);
+); 
+*/

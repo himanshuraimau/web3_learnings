3f82be41fcca9e2140d64a051337b0a59f0ea5ff
Himanshu Rai <himanshuraimau9@gmail.com>
2024-07-27 13:31:13 +0530

    add blinks

---
+++ blinks/testing-blinks/web/app/api/hello/route.ts
@@ -0,0 +1,30 @@
+import { ActionGetResponse, ActionPostRequest, ACTIONS_CORS_HEADERS, ActionGetRequest, ActionPostResponse } from "@solana/actions";
+import { NextResponse as Response } from "next/server";
+
+export async function GET(request: Request) {
+  const payload: ActionGetResponse = {
+    icon: "https://solana.com/_next/static/media/logotype.e4df684f.svg",
+    label: "Buy me a coffee",
+    description: "Buy me a coffee with SOL",
+    title: "Himanshu Rai-Buy me a coffee"
+  };
+
+  return Response.json(payload, {
+    headers: ACTIONS_CORS_HEADERS,
+  });
+}
+
+export async function POST(request: Request) {
+  const postRequest: ActionPostRequest = await request.json();
+  const userPubKey = postRequest.account;
+  console.log(userPubKey);
+
+  const response: ActionPostResponse = {
+    transaction: "",
+    message: "Some message"
+  };
+
+  return Response.json(response, {
+    headers: ACTIONS_CORS_HEADERS,
+  });
+}

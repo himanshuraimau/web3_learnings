ca8275a2445d7c4719904e49da8427cc8a2922b6
Himanshu Rai <himanshuraimau9@gmail.com>
2024-07-27 13:37:04 +0530

    add blink

--- blinks/testing-blinks/web/app/api/hello/route.ts
+++ blinks/testing-blinks/web/app/api/hello/route.ts
@@ -1,30 +1,29 @@
-import { ActionGetResponse, ActionPostRequest, ACTIONS_CORS_HEADERS, ActionGetRequest, ActionPostResponse } from "@solana/actions";
-import { NextResponse as Response } from "next/server";
+import { ActionGetResponse, ActionPostRequest, ACTIONS_CORS_HEADERS,ActionGetRequest, ActionPostResponse } from "@solana/actions"
 
-export async function GET(request: Request) {
-  const payload: ActionGetResponse = {
-    icon: "https://solana.com/_next/static/media/logotype.e4df684f.svg",
-    label: "Buy me a coffee",
-    description: "Buy me a coffee with SOL",
-    title: "Himanshu Rai-Buy me a coffee"
-  };
+export async function GET(request:Request){
+  
+  const payload:ActionGetResponse={
+          icon: "https://solana.com/_next/static/media/logotype.e4df684f.svg",
+          label:"Buy me a coffe",
+          description:"Buy me a coffee with SOL",
+          title:"Himanshu Rai-Buy me a coffee"
 
-  return Response.json(payload, {
-    headers: ACTIONS_CORS_HEADERS,
-  });
+  }
+    return Response.json(payload,{
+      headers:ACTIONS_CORS_HEADERS,
+      status:201
+    });
 }
 
-export async function POST(request: Request) {
-  const postRequest: ActionPostRequest = await request.json();
+export async function POST(request:Request) {
+    
+  const postRequest:ActionPostRequest = await request.json();
   const userPubKey = postRequest.account;
-  console.log(userPubKey);
+  console.log(userPubKey)
 
-  const response: ActionPostResponse = {
-    transaction: "",
-    message: "Some message"
-  };
-
-  return Response.json(response, {
-    headers: ACTIONS_CORS_HEADERS,
-  });
+  const response:ActionPostResponse ={
+    transaction:"",
+    message:"Some message"
+  }
+  return Response.json(response)
 }

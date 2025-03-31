050adbc28ed0bf6bd6ee6e955ce2525d06567ecd
Himanshu Rai <himanshuraimau9@gmail.com>
2025-03-31 18:52:49 +0530

    remove unused files and update .gitignore for sol_ dependencies

--- Web3-Cohort/Week2/wbw/README.md
+++
@@ -1,2 +0,0 @@
-A Web based Wallet Generator using Ether.
-Link: https://wallet-generator-sigma.vercel.app/
---
+++ sol_contr/src/main.rs
@@ -0,0 +1,43 @@
+// // in js trait is like interface
+// // in js struct is like class
+// // in js impl is like class methods
+// // in js impl is like class methods
+// trait Shape {
+//     fn area(&self) -> f64;
+// }
+
+// struct Rect {
+//     width: f64,
+//     height: f64,
+// }
+
+// struct Circle {
+//     radius: f64,
+// }
+
+// impl Shape for Rect {
+//     fn area(&self) -> f64 {
+//         self.width * self.height
+//     }
+// }
+
+// impl Shape for Circle {
+//     fn area(&self) -> f64 {
+//         std::f64::consts::PI * self.radius * self.radius
+//     }
+// }
+
+// fn main() {
+//     println!("Hello, world!");
+
+//     let rect = Rect {
+//         width: 10.0,
+//         height: 5.0,
+//     };
+//     println!("Area of rectangle: {}", rect.area());
+
+//     let circ: Circle = Circle { radius: 5.0 };
+//     println!("Area of circle: {}", circ.area());
+// }
+
+
